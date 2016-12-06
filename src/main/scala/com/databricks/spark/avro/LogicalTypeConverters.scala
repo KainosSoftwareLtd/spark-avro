d62f9c3ac3e5ee928a44678dbb72b2b35ff929a7
Shannon Holgate <sholgate1305@gmail.com>
2016-12-06 13:06:10 +0000

    decimal/logical-converter-skeleton: Adding method stubs

---
+++ src/main/scala/com/databricks/spark/avro/LogicalTypeConverters.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2014 Databricks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.databricks.spark.avro
+
+/**
+  * This object contains methods that are used to convert
+  * Avro Logical types to Spark SQL schemas and vice versa.
+  */
+private object LogicalTypeConverters {
+
+  private[avro] def convertFieldTypeToLogical = Unit
+
+  private[avro] def convertToLogicalValue = Unit
+
+  private[avro] def toSqlType = Unit
+
+  private[avro] def toSql = Unit
+}

6997fcdf21ae5d0a518368131139b94bc08eaed0
Adam Fallon <a.fallon@kainos.com>
2016-12-16 11:06:03 +0000

    Added Failing tests

--- src/main/scala/com/databricks/spark/avro/LogicalTypeConverters.scala
+++ src/main/scala/com/databricks/spark/avro/LogicalTypeConverters.scala
@@ -17,8 +17,6 @@ package com.databricks.spark.avro
 
 import java.math.BigDecimal
 import java.nio.ByteBuffer
-import java.sql.Timestamp
-
 import org.apache.avro.{Schema, _}
 import org.apache.avro.LogicalTypes.Decimal
 import org.apache.spark.sql.types._
@@ -27,17 +25,21 @@ import org.apache.spark.sql.types._
   * This object contains methods that are used to convert
   * Avro Logical types to Spark SQL schemas and vice versa.
   */
+
 private object LogicalTypeConverters {
 
   private[avro] def convertFieldTypeToLogical(): Unit = {
 
   }
 
-  private[avro] def convertToLogicalValue = Unit
+  private[avro] def convertToLogicalValue(item: Any): ByteBuffer = {
+    val bigItem = item.asInstanceOf[java.math.BigDecimal]
+    ByteBuffer.wrap(bigItem.unscaledValue().toByteArray)
+  }
 
-  private[avro] def toSqlType(logicalType: LogicalType): Option[DataType] = logicalType match {
-    case decimalType:Decimal  => Some(DecimalType(decimalType.getPrecision, decimalType.getScale))
-    case _                    => None
+  private[avro] def toSqlType(datatype: LogicalType): Option[DecimalType] = datatype match {
+    case decimal: Decimal => Some(DecimalType(decimal.getPrecision, decimal.getScale))
+    case _ => None
   }
 
   private[avro] def toSql(logicalType: LogicalType, item: Any, schema: Schema): Option[BigDecimal] = logicalType match {
